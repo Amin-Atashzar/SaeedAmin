5479ce93fcfc58907904eee5b217f586383e6284
Amin-Atashzar <49231593+Amin-Atashzar@users.noreply.github.com>
2019-04-12 17:41:43 +0430

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+// stateless Functional component
+import React, { Component } from "react";
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
